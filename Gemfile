e30ade0e27dc897f069ac1fd0a0a970c1abfb4bc
Maurice Ford <mauriceford@gmail.com>
2025-03-04 05:42:50 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "dotenv"
+gem "http"
+gem "ascii_charts"
+gem "activesupport"
--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,31 @@
 # Write your soltuion here!
+require "http"
+require "json"
+require "dotenv/load"
+require "ascii_charts"
+require "active_support/core_ext/string/inflections"
+
+gkey = ENV.fetch("GMAPS_KEY")
+pwkey = ENV.fetch("PIRATE_WEATHER_KEY")
+
+userinput = "chicago heights"
+
+# url sample
+googleurl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userinput.titleize + "&key=" + gkey
+
+ggraw = HTTP.get(googleurl)
+ggjson = JSON.parse(ggraw)
+s1 = ggjson.fetch("results")
+s2 = s1[0].fetch("geometry")
+s3 = s2.fetch("location")
+lat = s3.fetch("lat")
+lng = s3.fetch("lng")
+
+pw_url = "https://api.pirateweather.net/forecast/" + pwkey + "/" + lat.to_s + "," + lng.to_s
+
+pwraw = HTTP.get(pw_url)
+pwjson = JSON.parse(pwraw)
+
+
+
+pp pw_url
